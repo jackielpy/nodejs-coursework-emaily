33bc9b6cabf9a910524538dd90a1fdebde0b0b59
jackielpy <jackie.lee@fitch.com>
2017-10-04 16:03:37 +0800

    test git email

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 
@@ -7,5 +8,4 @@ app.get('/', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT);

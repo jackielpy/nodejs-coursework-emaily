37d62bc6d41cfa95080942670b355221de48bc5d
jackielpy <jackie.lee@fitch.com>
2018-02-25 22:03:22 +0800

    google auth

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const PORT = process.env.PORT || 5000;
 
 // App constants
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+// App modules
+require('./services/passport');
+
+// App setup
+mongoose.connect(keys.mongoUri, {
+  user: "admin",
+  pass: "Pa$$w0rd"
+}, function(err, db) {});
+
 const app = express();
+
+require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
   res.send({
